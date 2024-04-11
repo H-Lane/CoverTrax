1addda723fa2d2d177bc93f5012d02f9c9b8153a
Hersey <helane1996@gmail.com>
2024-04-11 18:57:03 -0400

    updated code for common naming principles

--- assets/JS/cardPopulation.js
+++ assets/JS/cardPopulation.js
@@ -1,20 +1,20 @@
- const coverCardContainer = document.getElementById(`cover-cards-container`)
+ const coverCardContainerEl = document.getElementById(`cc-container`)
 
 
  function populateCoverCards(data) { //Pass this function in the API fetch request with the data parameter to populate the cards for the cover songs
 
     //Create a for loop that loops through the data and for each object populate the following elements
 
-    const coverCardBody = document.createElement(`div`); //Element witch holds card content
-    const coverCardArt = document.createElement(`img`); //Album or Song art
-    const coverCardTitle = document.createElement(`h3`); //Name of Song
-    const coverCardArtist = document.createElement(`h4`); //Name of Artist
-    const coverCardLink = document.createElement(`p`); //Spotify link to song
+    const ccBodyEl = document.createElement(`div`); //Element which holds card content
+    const ccArtEl = document.createElement(`img`); //Album or Song art
+    const ccTitleEl = document.createElement(`h3`); //Name of Song
+    const ccArtistEl = document.createElement(`h4`); //Name of Artist
+    const ccLinkEl = document.createElement(`p`); //Spotify link to song
 
 
-    coverCardBody.appendChild(coverCardArt);
-    coverCardBody.appendChild(coverCardTitle);
-    coverCardBody.appendChild(coverCardArtist);
-    coverCardBody.appendChild(coverCardLink);
-    coverCardContainer.appendChild(coverCardBody)
+    ccBodyEl.appendChild(ccArtEl);
+    ccBodyEl.appendChild(ccTitleEl);
+    ccBodyEl.appendChild(ccArtistEl);
+    ccBodyEl.appendChild(ccLinkEl);
+    coverCardContainerEl.appendChild(ccBodyEl)
  }

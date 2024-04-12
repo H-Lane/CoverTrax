ce9fd779e68a7b6eb70cc583dd0a825cfbc9272b
Hersey <helane1996@gmail.com>
2024-04-11 20:44:55 -0400

    began creating the dynamic additions to the cards

--- assets/JS/cardPopulation.js
+++ assets/JS/cardPopulation.js
@@ -4,13 +4,14 @@
  function populateCoverCards(data) { //Pass this function in the API fetch request with the data parameter to populate the cards for the cover songs
 
     //Create a for loop that loops through the data and for each object populate the following elements
-
+    for (let i = 1, i < `11`, i++ )
     const ccBodyEl = document.createElement(`div`); //Element which holds card content
     const ccArtEl = document.createElement(`img`); //Album or Song art
     const ccTitleEl = document.createElement(`h3`); //Name of Song
     const ccArtistEl = document.createElement(`h4`); //Name of Artist
     const ccLinkEl = document.createElement(`p`); //Spotify link to song
 
+    ccArtEl.textContent = data.album
 
     ccBodyEl.appendChild(ccArtEl);
     ccBodyEl.appendChild(ccTitleEl);

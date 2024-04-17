caf2c36676eadc936718df26fd0d4015f550e846
Hersey <helane1996@gmail.com>
2024-04-17 17:15:48 -0400

    Added function to populate ot cards ready for input into Hunters function. Just need id names on html elements

--- assets/JS/cardPopulation.js
+++ assets/JS/cardPopulation.js
@@ -1,6 +1,6 @@
 const coverCardContainerEl = document.getElementById(`cc-container`);
 const otLyricsContainer = document.getElementById(`ot-lyrics-container`);
-
+const otCardContainerEl = document.getElementById(`ot-container`);
 
 // let test = {
 //     items: [
@@ -33,7 +33,6 @@ function populateCoverCards(data) {
   console.log(data);
   coverCardContainerEl.innerHTML = "";
 
-
   for (let i = 1; i < data.tracks.items.length; i++) {
     const ccDivEl = document.createElement(`div`); //Element that Holds each Card for CSS purpose
     const ccBodyEl = document.createElement(`div`); //Element which holds card content
@@ -49,10 +48,9 @@ function populateCoverCards(data) {
     ccTitleEl.classList.add(`uk-card-title`);
     ccArtContEl.classList.add(`uk-card-media-top`);
 
-    
     ccArtEl.src = data.tracks.items[i].album.images[0].url; //populate song art
     ccTitleEl.textContent = data.tracks.items[i].name; //populate song title
-    ccLinkEl.textContent = data.tracks.items[i].external_urls.spotify;//populate spotify song link
+    ccLinkEl.textContent = data.tracks.items[i].external_urls.spotify; //populate spotify song link
 
     ccArtistEl.textContent = `By: `; //Populate Artists names. The following loop handles multiple artists if there is more than one.
 
@@ -76,18 +74,56 @@ function populateCoverCards(data) {
     ccBodyEl.appendChild(ccArtContEl);
     ccBodyEl.appendChild(ccArtistEl);
     ccBodyEl.appendChild(ccLinkEl);
-    ccDivEl.appendChild(ccBodyEl)
+    ccDivEl.appendChild(ccBodyEl);
     coverCardContainerEl.appendChild(ccDivEl);
     if (i === 9) break;
   }
-};
+}
 
-function populateLyrics(data){ //populate lyrics onto their HTML container
+function populateLyrics(data) {
+  //populate lyrics onto their HTML container
   const otLyricsEl = document.getElementById(`ot-lyrics`);
   otLyricsEl.textContent = data.message.body.lyrics.lyrics_body;
   otLyricsContainer.appendChild(otLyricsEl);
   console.log(data);
-};
+}
 
+function populateOtCard(data) {
+  const otDivEl = document.getElementById(``); //Element that points to the container for the ot card
+  const otBodyEl = document.getElementById(``); //Element points to the the content for the ot card
+  const otArtContEl = document.getElementById(``); //Element that has the style container for the art
+  const otArtEl = document.getElementById(``); //Element that points to the album art
+  const otTitleEl = document.getElementById(``); //Element that points to the song name
+  const otArtistEl = document.getElementById(``); //Element that points to the artists names
+  const otLinkEl = document.getElementById(``); //Element that points to the spotify link
+
+  otArtEl.src = data.tracks.items[0].album.images[0].url; //populate song art
+  otTitleEl.textContent = data.tracks.items[0].name; //populate song title
+  otLinkEl.textContent = data.tracks.items[0].external_urls.spotify; //populate spotify song link
+
+  otArtistEl.textContent = `By: `; //Populate Artists names. The following loop handles multiple artists if there is more than one.
+
+  for (let j = 0; j < data.tracks.items[0].artists.length; j++) {
+    if (
+      j === data.tracks.items[0].artists.length - 1 &&
+      data.tracks.items[0].artists.length > 1
+    ) {
+      otArtistEl.textContent += "and " + data.tracks.items[0].artists[j].name;
+    } else if (data.tracks.items[0].artists.length === 2) {
+      otArtistEl.textContent += data.tracks.items[0].artists[j].name + " ";
+    } else if (data.tracks.items[0].artists.length === 1) {
+      otArtistEl.textContent += data.tracks.items[0].artists[j].name;
+    } else {
+      otArtistEl.textContent += data.tracks.items[0].artists[j].name + ", ";
+    }
+  }
+
+  otArtContEl.appendChild(otArtEl);
+  otBodyEl.appendChild(otTitleEl);
+  otBodyEl.appendChild(otArtContEl);
+  otBodyEl.appendChild(otArtistEl);
+  otBodyEl.appendChild(otLinkEl);
+  otDivEl.appendChild(otBodyEl);
+  otCardContainerEl.appendChild(otDivEl);
+}
 // On page load, pull the most recent search term out of the local storage array using array.length - 1, then use that term to pass into our fetch requests to populate the page and run the populate functions.
-

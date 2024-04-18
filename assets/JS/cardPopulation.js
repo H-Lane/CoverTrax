ac998c9032e4f3854c6c550a2ce838cd44c0f19a
Mike Ferg <mikeferguson@designprollc.com>
2024-04-17 20:41:14 -0400

    added search bar suggestions

--- assets/JS/cardPopulation.js
+++ assets/JS/cardPopulation.js
@@ -1,7 +1,8 @@
 const coverCardContainerEl = document.getElementById(`cc-container`);
 const otLyricsContainer = document.getElementById(`ot-lyrics-container`);
-
-
+// El = Element
+// cc = cover container
+// ot = original track
 // let test = {
 //     items: [
 //         {
@@ -33,7 +34,7 @@ function populateCoverCards(data) {
   console.log(data);
   coverCardContainerEl.innerHTML = "";
 
-
+  // start
   for (let i = 1; i < data.tracks.items.length; i++) {
     const ccDivEl = document.createElement(`div`); //Element that Holds each Card for CSS purpose
     const ccBodyEl = document.createElement(`div`); //Element which holds card content
@@ -41,19 +42,29 @@ function populateCoverCards(data) {
     const ccArtEl = document.createElement(`img`); //Album or Song art
     const ccTitleEl = document.createElement(`h3`); //Name of Song
     const ccArtistEl = document.createElement(`h4`); //Name of Artist
-    const ccLinkEl = document.createElement(`p`); //Spotify link to song
+    const ccLinkEl = document.createElement(`a`); //Spotify link to song
 
     //MIKE ADD CLASSES HERE
-    ccBodyEl.classList.add(`uk-card`, `uk-card-body`, `uk-card-default`);
+    ccDivEl.classList.add(
+      `uk-card`,
+      `uk-card-body`,
+      `uk-card-default`,
+      `uk-card-media-top`,
+      `uk-card-title`
+    );
+    ccBodyEl.classList.add(`uk-card`, `uk-card-body`, `uk-card-media-top`);
+    ccArtContEl.classList.add(`uk-card-media-top`);
     ccArtEl.classList.add(`none`);
     ccTitleEl.classList.add(`uk-card-title`);
-    ccArtContEl.classList.add(`uk-card-media-top`);
+    ccArtistEl.classList.add();
+    ccLinkEl.classList.add(`lyrics-button`);
 
-    
     ccArtEl.src = data.tracks.items[i].album.images[0].url; //populate song art
     ccTitleEl.textContent = data.tracks.items[i].name; //populate song title
-    ccLinkEl.textContent = data.tracks.items[i].external_urls.spotify;//populate spotify song link
-
+    ccLinkEl.setAttribute("href", data.tracks.items[i].external_urls.spotify);
+    ccLinkEl.setAttribute("target", "_blank"); //Opens a new tab when clicked
+    ccLinkEl.textContent = "Link to song"; //populate spotify song link
+    // ccLinkEl.textContent = data.tracks.items[i].external_urls.spotify
     ccArtistEl.textContent = `By: `; //Populate Artists names. The following loop handles multiple artists if there is more than one.
 
     for (let j = 0; j < data.tracks.items[i].artists.length; j++) {
@@ -76,18 +87,18 @@ function populateCoverCards(data) {
     ccBodyEl.appendChild(ccArtContEl);
     ccBodyEl.appendChild(ccArtistEl);
     ccBodyEl.appendChild(ccLinkEl);
-    ccDivEl.appendChild(ccBodyEl)
+    ccDivEl.appendChild(ccBodyEl);
     coverCardContainerEl.appendChild(ccDivEl);
     if (i === 9) break;
   }
-};
+}
 
-function populateLyrics(data){ //populate lyrics onto their HTML container
+function populateLyrics(data) {
+  //populate lyrics onto their HTML container
   const otLyricsEl = document.getElementById(`ot-lyrics`);
   otLyricsEl.textContent = data.message.body.lyrics.lyrics_body;
   otLyricsContainer.appendChild(otLyricsEl);
   console.log(data);
-};
+}
 
 // On page load, pull the most recent search term out of the local storage array using array.length - 1, then use that term to pass into our fetch requests to populate the page and run the populate functions.
-
